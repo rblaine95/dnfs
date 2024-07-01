baa0616b720de2b10d32e3c90a6994309a6b83cf
Robbie Blaine <robbieblaine.rb@gmail.com>
2024-07-01 21:49:56 +0200

    Clap and Color-Eyre
    * Nicer error messages via color-eyre
    * Use Clap to build a simple CLI

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,15 @@
 // This is extremely safe, it says so right here!
 #![forbid(unsafe_code)]
 
-use std::path::Path;
+use std::{io, path::Path};
 
-use anyhow::{anyhow, Result};
 use base64::prelude::*;
+use clap::{Parser, Subcommand};
 use cloudflare::{
     endpoints::dns,
     framework::{async_api, auth, Environment, HttpApiClientConfig},
 };
+use color_eyre::eyre::{OptionExt, Result};
 use securefmt::Debug;
 use serde_derive::{Deserialize, Serialize};
 use thiserror::Error;
@@ -21,7 +22,10 @@ use trust_dns_resolver::{
 };
 
 const USAGE_AGREEMENT: &str = "I understand that DNFS is a terrible idea and I promise I will never use it for anything important ever";
-const MAX_CHUNK_SIZE: usize = 1024;
+// Max TXT Content per record is 2048 characters
+// Base64 encoding by ±33% (for every 3 bytes of input, you get 4 bytes out)
+const MAX_TXT_CONTENT_SIZE: usize = 2048;
+const MAX_CHUNK_SIZE: usize = (MAX_TXT_CONTENT_SIZE * 3) / 4;
 
 #[derive(Debug, Clone)]
 struct File {
@@ -43,7 +47,7 @@ impl File {
     fn new(path: &Path) -> Result<Self> {
         let name = path
             .file_name()
-            .ok_or_else(|| anyhow!("Invalid file name"))?
+            .ok_or_eyre("Invalid file name")?
             .to_string_lossy()
             .into_owned();
         let data = std::fs::read(path)?;
@@ -135,7 +139,7 @@ impl File {
         let file_sha256 = sha256::digest(file_data.as_slice()).to_string();
 
         if file_sha256 != file_record.sha256 {
-            return Err(anyhow!("File integrity check failed"));
+            return Err(DNFSError::InvalidSHA256(file_record.sha256).into());
         }
 
         // parse `file_data` to `File` struct
@@ -269,7 +273,7 @@ impl FileRecord {
             .iter()
             .flat_map(TXT::txt_data)
             .find_map(|txt_data| std::str::from_utf8(txt_data).ok())
-            .ok_or_else(|| anyhow!("Error parsing TXT record"))?;
+            .ok_or_else(|| DNFSError::ParseError(file_fqdn.to_string()))?;
 
         debug!("File Data: {file_txt:?}");
 
@@ -330,7 +334,7 @@ impl FileRecord {
                 })
                 .await?;
         } else {
-            return Err(anyhow!("Record not found"));
+            return Err(DNFSError::RecordNotFound(file_fqdn.to_string()).into());
         }
         Ok(())
     }
@@ -389,7 +393,7 @@ impl Config {
     fn new(path: &Path) -> Result<Self> {
         match toml::from_str(std::fs::read_to_string(path)?.as_str()) {
             Ok(config) => Ok(config),
-            Err(e) => Err(anyhow!("Error parsing config file: {}", e)),
+            Err(e) => Err(DNFSError::ConfigError(e.to_string()).into()),
         }
     }
 }
@@ -399,6 +403,14 @@ impl Config {
 pub enum DNFSError {
     #[error("Invalid DNFS usage agreement: {0}")]
     InvalidUsageAgreement(String),
+    #[error("Invalid SHA256 hash: {0}")]
+    InvalidSHA256(String),
+    #[error("Error parsing DNS record: {0}")]
+    ParseError(String),
+    #[error("Error parsing Config file: {0}")]
+    ConfigError(String),
+    #[error("Record not found: {0}")]
+    RecordNotFound(String),
 }
 
 async fn get_record_id(
@@ -476,7 +488,7 @@ async fn check_usage_agreement(
     resolver: &AsyncResolver<GenericConnector<TokioRuntimeProvider>>,
 ) -> Result<()> {
     let usage_agreement_host = format!("_dnfs-agreement.{domain_name}");
-    let usage_agreement = resolver.txt_lookup(usage_agreement_host).await?;
+    let usage_agreement = resolver.txt_lookup(usage_agreement_host.clone()).await?;
 
     usage_agreement
         .iter()
@@ -492,15 +504,36 @@ async fn check_usage_agreement(
                 }
             })
         })
-        .unwrap_or_else(|| {
-            Err(anyhow!(
-                "Usage agreement not found or does not match expected text"
-            ))
-        })
+        .unwrap_or_else(|| Err(DNFSError::InvalidUsageAgreement(usage_agreement_host).into()))
+}
+
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+struct Cli {
+    #[command(subcommand)]
+    command: Commands,
+}
+
+#[derive(Subcommand, Debug)]
+enum Commands {
+    Upload {
+        #[arg(help = "The file to upload")]
+        path: String,
+    },
+    Download {
+        #[arg(help = "The file to download")]
+        fqdn: String,
+    },
+    Delete {
+        #[arg(help = "The file to delete")]
+        fqdn: String,
+    },
 }
 
 #[tokio::main]
 async fn main() -> Result<()> {
+    color_eyre::install()?;
+
     // Default log level is `info`
     if std::env::var("RUST_LOG").is_err() {
         std::env::set_var(
@@ -510,11 +543,20 @@ async fn main() -> Result<()> {
     }
     tracing_subscriber::fmt()
         .with_env_filter(tracing_subscriber::EnvFilter::from_default_env())
+        .with_writer(io::stderr)
         .init();
 
     // Load config
     let config = Config::new(Path::new("config.toml"))?;
     debug!("{config:?}");
+
+    let resolver =
+        TokioAsyncResolver::tokio(ResolverConfig::cloudflare_tls(), ResolverOpts::default());
+
+    // Check usage agreement
+    if let Err(e) = check_usage_agreement(&config.dnfs.domain_name, &resolver).await {
+        panic!("Error checking usage agreement: {e}");
+    }
 
     let cf_client = async_api::Client::new(
         auth::Credentials::UserAuthToken {
@@ -524,32 +566,29 @@ async fn main() -> Result<()> {
         Environment::Production,
     )?;
 
-    let resolver =
-        TokioAsyncResolver::tokio(ResolverConfig::cloudflare_tls(), ResolverOpts::default());
-
-    // Check usage agreement
-    if let Err(e) = check_usage_agreement(&config.dnfs.domain_name, &resolver).await {
-        error!("Error checking usage agreement: {}", e);
-        std::process::exit(1);
+    let cli = Cli::parse();
+    match &cli.command {
+        Commands::Upload { path } => {
+            let file = File::new(Path::new(path))?;
+            let file_upload = file
+                .upload(
+                    &cf_client,
+                    &config.cloudflare.zone_id,
+                    &config.dnfs.domain_name,
+                )
+                .await?;
+            println!("File successfully uploaded - {file_upload}");
+        }
+        Commands::Download { fqdn } => {
+            let file = File::read(fqdn, &resolver).await?;
+            let file_data = file.read_to_string();
+            println!("{file_data}");
+        }
+        Commands::Delete { fqdn } => {
+            FileRecord::delete(fqdn, &cf_client, &config.cloudflare.zone_id, &resolver).await?;
+            println!("File successfully deleted - {fqdn}");
+        }
     }
-
-    // Read `test.txt` into a `File` struct
-    let test = File::new(Path::new("test.txt"))?;
-    let test_upload = test
-        .upload(
-            &cf_client,
-            &config.cloudflare.zone_id,
-            &config.dnfs.domain_name,
-        )
-        .await?;
-    println!("{test_upload}");
-
-    // Read `test.dnfs.bunkerlab.net` into a `File` struct
-    let test_file_data = (File::read("test.dnfs.bunkerlab.net", &resolver).await?).read_to_string();
-    println!("{test_file_data}");
-
-    // Read `rfc1464.txt` into a `File` struct
-    // let rfc1464 = File::new(Path::new("rfc1464.txt"))?;
 
     Ok(())
 }
