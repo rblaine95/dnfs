9e1ce5f8c36c09a1af73f683c313c983253fa9a8
Robbie Blaine <robbieblaine.rb@gmail.com>
2024-07-02 15:50:20 +0200

    Simple Compression

--- src/file.rs
+++ src/file.rs
@@ -43,6 +43,7 @@ impl File {
             .to_string_lossy()
             .into_owned();
         let data = std::fs::read(path)?;
+        let compressed_data = snap::raw::Encoder::new().compress_vec(&data)?;
         let extension = path
             .extension()
             .map(|ext| ext.to_string_lossy().into_owned());
@@ -53,7 +54,7 @@ impl File {
         let sha256 = sha256::digest(data.as_slice()).to_string();
 
         Ok(Self {
-            data: data
+            data: compressed_data
                 .chunks(MAX_CHUNK_SIZE)
                 .enumerate()
                 .map(|(index, data)| Chunk {
@@ -114,7 +115,7 @@ impl File {
     }
 
     // Given `file_name.dnfs.domain_name TXT "v=dnfs1 chunks=1 size=12 sha256hash=d2a84f4b8b650937ec8f73cd8be2c74add5a911ba64df27458ed8229da804a26 mime=text/plain extension=txt"`
-    // Reconstruct the file from its chunks, verify the sha256 hash, and output to STDOUT
+    // Reconstruct the file from its chunks and verify the sha256 hash
     pub async fn read(
         file_fqdn: &str,
         resolver: &AsyncResolver<GenericConnector<TokioRuntimeProvider>>,
@@ -123,18 +124,20 @@ impl File {
 
         let file_record = FileRecord::from_dns_record(file_fqdn, resolver).await?;
         let file_chunks = File::get_chunks(file_fqdn, &file_record, resolver).await?;
-        let file_data = file_chunks.iter().fold(Vec::new(), |mut acc, chunk| {
+
+        // Calculate and validate SHA256 hash
+        let compressed_data = file_chunks.iter().fold(Vec::new(), |mut acc, chunk| {
             acc.extend_from_slice(&chunk.data);
             acc
         });
-
-        let file_sha256 = sha256::digest(file_data.as_slice()).to_string();
+        let uncompressed_data = snap::raw::Decoder::new().decompress_vec(&compressed_data)?;
+        let file_sha256 = sha256::digest(uncompressed_data.as_slice()).to_string();
 
         if file_sha256 != file_record.sha256 {
             return Err(DNFSError::InvalidSHA256(file_record.sha256).into());
         }
 
-        // parse `file_data` to `File` struct
+        // parse to `File` struct
         let file = File {
             data: file_chunks,
             extension: file_record.extension,
@@ -149,10 +152,14 @@ impl File {
     }
 
     pub fn read_to_string(&self) -> String {
-        self.data.iter().fold(String::new(), |mut acc, chunk| {
-            acc.push_str(std::str::from_utf8(&chunk.data).unwrap());
+        let compressed = self.data.iter().fold(Vec::new(), |mut acc, chunk| {
+            acc.extend_from_slice(&chunk.data);
             acc
-        })
+        });
+        let uncompressed = snap::raw::Decoder::new()
+            .decompress_vec(&compressed)
+            .unwrap();
+        String::from_utf8_lossy(&uncompressed).to_string()
     }
 
     async fn get_chunks(
